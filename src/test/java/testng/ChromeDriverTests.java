b1b0b2fa0edb7ca8d1d8608fa062c1414c41d1fa
Volodymyr Protsiv <Volodymyr_Protsiv@epam.com>
2021-05-17 20:52:23 +0300

    empty lines deleted

--- src/test/java/testng/ChromeDriverTests.java
+++ src/test/java/testng/ChromeDriverTests.java
@@ -57,4 +57,3 @@ public class ChromeDriverTests extends BaseTest {
         Assert.assertTrue(contentOfWikipediaSearchResult.contains("Test Automation"), "The search results are not contain expected text.");
     }
 }
-
