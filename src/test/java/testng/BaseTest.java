f0b1f685a35eed6f421e6b75e4de9ea68453a5e1
Volodymyr Protsiv <Volodymyr_Protsiv@epam.com>
2021-05-17 16:47:28 +0300

    Create BaseTest.java

---
+++ src/test/java/testng/BaseTest.java
@@ -0,0 +1,30 @@
+package testng;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+    WebDriver driver;
+    Logger logger = Logger.getLogger(BaseTest.class);
+
+    @BeforeClass
+    public void browserSetUp() {
+        driver = new ChromeDriver();
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
+        driver = new FirefoxDriver();
+    }
+
+    @AfterClass
+    public void browserTearDown() {
+        driver.quit();
+        driver = null;
+    }
+}
+
